14efef1b58c83fc1f857428edaf9028d390eacbe
Galvin Radley Ngo <51685547+galvinradleyngo@users.noreply.github.com>
2025-08-19 15:35:32 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// IMPORTANT: change REPO-NAME to your repository name
 export default defineConfig({
   plugins: [react()],
+  base: '/dart/',
 })
