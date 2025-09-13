e26d666951e1f1f0e0277b6a01cc1f4fcca55c62
Galvin Radley Ngo <51685547+galvinradleyngo@users.noreply.github.com>
2025-09-13 09:39:06 +0800

    fix: guard action menu with ternary

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      screens: {
+        xs: "475px",
+      },
+    },
+  },
   plugins: [],
 };
