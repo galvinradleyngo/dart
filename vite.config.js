7f93077f0a58ab0d39ae23c8d599ac64eff1e4b9
Galvin Radley Ngo <51685547+galvinradleyngo@users.noreply.github.com>
2026-06-08 07:03:06 +0000

    fixdart

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isGitHubPagesBuild = process.env.GITHUB_ACTIONS === 'true'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: isGitHubPagesBuild ? '/dart/' : '/',
 })
